329c85ddc81778f2c7e7453c2c8076b6cb7284bd
yuminmi <minming0400@naver.com>
2025-10-29 09:54:57 +0900

    feat: bottomsheetmodal 적용, 카테고리, 위치 선택 모달창 구현

--- mobile/screens/PostListScreen.js
+++ mobile/screens/PostListScreen.js
@@ -8,10 +8,17 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import DefaultHeader from "../components/DefaultHeader";
 import PostTypeSelector from "../components/PostTypeSelector";
 import PostListItem from "../components/PostListItem";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import CategoryList from "../components/CategoryList";
 
 const POSTS = [
   {
@@ -77,63 +84,143 @@ const POSTS = [
 ];
 
 const PostListScreen = () => {
+  // bottomSheet
+  const bottomSheetCategoryModalRef = useRef(null);
+  const handleCategoryModalPress = useCallback(() => {
+    bottomSheetCategoryModalRef.current?.present();
+  }, []);
+  const handleCategorySheetChanges = useCallback((index) => {
+    console.log("bottomSheetCategoryChanges", index);
+  }, []);
+  const bottomSheetLocationModalRef = useRef(null);
+  const handleLocationModalPress = useCallback(() => {
+    bottomSheetLocationModalRef.current?.present();
+  }, []);
+  const handleLocationSheetChanges = useCallback((index) => {
+    console.log("bottomSheetLocationChanges", index);
+  }, []);
+
   const [postType, setPostType] = useState("acquired"); // acquired lost all
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <DefaultHeader />
-      <View style={styles.listContainer}>
-        <PostTypeSelector postType={postType} setPostType={setPostType} />
-        <View>
-          <Pressable style={[styles.filterResetBtn]}>
-            <Image
-              source={require("../assets/filterReset.png")}
-              style={styles.resetImg}
-            ></Image>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>
-              필터 초기화
-            </Text>
-          </Pressable>
-        </View>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          style={styles.filterBtnContainer}
-          contentContainerStyle={styles.filterBtnContent}
-        >
-          <Pressable style={[styles.filterDownBtn]}>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>카테고리</Text>
-            <Image
-              source={require("../assets/downArrow.png")}
-              style={styles.filterDownImg}
-            ></Image>
-          </Pressable>
-          <Pressable style={[styles.filterDownBtn]}>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>위치</Text>
-            <Image
-              source={require("../assets/downArrow.png")}
-              style={styles.filterDownImg}
-            ></Image>
-          </Pressable>
-          <Pressable style={[styles.filterBtn]}>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>미완료</Text>
-          </Pressable>
-          <Pressable style={[styles.filterBtn]}>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>완료</Text>
-          </Pressable>
-          <Pressable style={[styles.filterBtn]}>
-            <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>인계됨</Text>
-          </Pressable>
-        </ScrollView>
-        <FlatList
-          data={POSTS}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <PostListItem post={item} />}
-          showsVerticalScrollIndicator={false}
-          style={{ flex: 1 }}
-          contentContainerStyle={{ padding: 2 }}
-        />
-      </View>
-    </SafeAreaView>
+    <GestureHandlerRootView>
+      <BottomSheetModalProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <DefaultHeader />
+          <View style={styles.listContainer}>
+            <PostTypeSelector postType={postType} setPostType={setPostType} />
+            <View>
+              <Pressable style={[styles.filterResetBtn]}>
+                <Image
+                  source={require("../assets/filterReset.png")}
+                  style={styles.resetImg}
+                ></Image>
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>
+                  필터 초기화
+                </Text>
+              </Pressable>
+            </View>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              style={styles.filterBtnContainer}
+              contentContainerStyle={styles.filterBtnContent}
+            >
+              <Pressable
+                onPress={handleCategoryModalPress}
+                style={[styles.filterDownBtn]}
+              >
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>
+                  카테고리
+                </Text>
+                <Image
+                  source={require("../assets/downArrow.png")}
+                  style={styles.filterDownImg}
+                ></Image>
+              </Pressable>
+              <Pressable
+                onPress={handleLocationModalPress}
+                style={[styles.filterDownBtn]}
+              >
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>위치</Text>
+                <Image
+                  source={require("../assets/downArrow.png")}
+                  style={styles.filterDownImg}
+                ></Image>
+              </Pressable>
+              <Pressable style={[styles.filterBtn]}>
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>
+                  미완료
+                </Text>
+              </Pressable>
+              <Pressable style={[styles.filterBtn]}>
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>완료</Text>
+              </Pressable>
+              <Pressable style={[styles.filterBtn]}>
+                <Text style={[styles.BtnText, { color: "#a8a8a8" }]}>
+                  인계됨
+                </Text>
+              </Pressable>
+            </ScrollView>
+            <FlatList
+              data={POSTS}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => <PostListItem post={item} />}
+              showsVerticalScrollIndicator={false}
+              style={{ flex: 1 }}
+              contentContainerStyle={{ padding: 2 }}
+            />
+            <BottomSheetModal
+              ref={bottomSheetCategoryModalRef}
+              onChange={handleCategorySheetChanges}
+              style={styles.bottomSheetModal}
+            >
+              <BottomSheetView style={styles.contentContainer}>
+                <View style={styles.bottomModalContentTitle}>
+                  <Text style={styles.bottomModalContentTitleText}>
+                    물품 카테고리
+                  </Text>
+                </View>
+                <CategoryList />
+                <View style={styles.bottomModalBtnContainer}>
+                  <Pressable style={styles.bottomModalResetBtn}>
+                    <Text>초기화</Text>
+                  </Pressable>
+                  <Pressable style={styles.bottomModalSubmitBtn}>
+                    <Text style={styles.submitBtnText}>적용</Text>
+                  </Pressable>
+                </View>
+              </BottomSheetView>
+            </BottomSheetModal>
+            <BottomSheetModal
+              ref={bottomSheetLocationModalRef}
+              onChange={handleLocationSheetChanges}
+              style={styles.bottomSheetModal}
+            >
+              <BottomSheetView style={styles.contentContainer}>
+                <View style={styles.bottomModalContentTitle}>
+                  <Text style={styles.bottomModalContentTitleText}>위치</Text>
+                </View>
+                <View style={styles.locationSelectMask}>
+                  <Image
+                    source={require("../assets/inhaMap.png")}
+                    style={styles.locationMapImg}
+                  ></Image>
+                </View>
+                <View style={styles.bottomModalBtnContainer}>
+                  <Pressable style={styles.bottomModalResetBtn}>
+                    <Text>초기화</Text>
+                  </Pressable>
+                  <Pressable style={styles.bottomModalSubmitBtn}>
+                    <Text style={styles.submitBtnText}>적용</Text>
+                  </Pressable>
+                </View>
+              </BottomSheetView>
+            </BottomSheetModal>
+          </View>
+        </SafeAreaView>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
 
@@ -189,5 +276,52 @@ const styles = StyleSheet.create({
   filterBtnContent: {
     flexDirection: "row",
     alignItems: "center",
+  },
+  bottomSheetModal: {
+    borderRadius: 25,
+  },
+  contentContainer: {
+    padding: 20,
+    paddingTop: 10,
+  },
+  bottomModalContentTitle: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#d4d4d4",
+    alignItem: "center",
+    padding: 10,
+  },
+  bottomModalContentTitleText: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  bottomModalResetBtn: {
+    borderRadius: 10,
+    backgroundColor: "#e9e9e9",
+    padding: 10,
+    flex: 2,
+    alignItems: "center",
+  },
+  bottomModalSubmitBtn: {
+    borderRadius: 10,
+    backgroundColor: "#2165A6",
+    padding: 10,
+    flex: 8,
+    alignItems: "center",
+  },
+  bottomModalBtnContainer: {
+    flexDirection: "row",
+    gap: 14,
+  },
+  submitBtnText: {
+    color: "white",
+  },
+  locationSelectMask: {
+    width: 380,
+    height: 230,
+    marginVertical: 15,
+  },
+  locationMapImg: {
+    width: "100%",
+    height: "100%",
   },
 });
