f0ac255beaeebf0a01d6ca8fe9af772e3c36a1d5
yuminmi <minming0400@naver.com>
2025-10-29 14:59:43 +0900

    refactor: web파일 삭제, 알림창 구현

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import MainScreen from "./screens/MainScreen";
 import PostListScreen from "./screens/PostListScreen";
+import NotificationListScreen from "./screens/NotificationListScreen";
 
 export default function App() {
-  return <PostListScreen />;
+  return <NotificationListScreen />;
 }
 
 const styles = StyleSheet.create({
